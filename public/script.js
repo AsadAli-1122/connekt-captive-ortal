d6365eeb8c54bbdfc115f9f2355fd4743cb617d7
AsadAli-1122 <asadali.cool.23@gmail.com>
2023-07-07 20:02:59 +0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,197 @@
+// public/script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  const section1 = document.getElementById('section_1');
+  const section2 = document.getElementById('section_2');
+  const section3 = document.getElementById('section_3');
+  const section4 = document.getElementById('section_4');
+  const myButton = document.getElementById('login');
+  const sendOtpButton = document.getElementById('send_otp');
+  const verifyOtpButton = document.getElementById('verify_otp');
+  const nameInput = document.getElementById('name');
+  const numberInput = document.getElementById('number');
+  const otpInput = document.getElementById('otp_input');
+  const errorName = document.getElementById('error_name');
+  const errorNumber = document.getElementById('error_number');
+  const errorOtp = document.getElementById('error_otp');
+  let otpCode = "";
+
+  myButton.addEventListener('click', function() {
+    section1.style.display = 'none';
+    section2.style.display = 'block';
+    playRandomVideo();
+  });
+
+  function playRandomVideo() {
+    const videoSources = [
+      './video/video_1.mp4',
+      './video/video_2.mp4',
+      './video/video_3.mp4',
+      './video/video_4.mp4'
+    ];
+    const randomIndex = Math.floor(Math.random() * videoSources.length);
+    const randomVideoSource = videoSources[randomIndex];
+    const myVideo = document.getElementById('myVideo');
+    myVideo.src = randomVideoSource;
+    myVideo.load();
+    myVideo.play();
+  }
+
+  const myVideo = document.getElementById('myVideo');
+  myVideo.addEventListener('ended', function() {
+    section2.style.display = 'none';
+    section3.style.display = 'block';
+  });
+
+  sendOtpButton.addEventListener('click', async function(e) {
+    e.preventDefault();
+
+    const username = nameInput.value;
+    const number = numberInput.value;
+  
+    let hasError = false; // Track if there are any errors
+  
+    if (username.trim() === '') {
+      showErrorName('Username is required.');
+      hasError = true;
+    } else if (!validateName(username)) {
+      showErrorName('Username must be 4 to 19 characters.');
+      hasError = true;
+    } else {
+      hideErrorName(); // Username is valid, remove the error message
+    }
+  
+    if (number.trim() === '') {
+      showErrorNumber('Phone number is required.');
+      hasError = true;
+    } else if (!validateNumber(number)) {
+      showErrorNumber('Invalid number format. Please use the format 923001234567.');
+      hasError = true;
+    } else {
+      hideErrorNumber(); // Number is valid, remove the error message
+    }
+  
+    if (hasError) {
+      return; // Stop further execution if there are any errors
+    }
+
+
+
+    otpCode = generateOTP();
+
+    // Send the verification code via Twilio SMS
+    try {
+      const response = await fetch('/send-otp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          number: number,
+          code: otpCode,
+        }),
+      });
+
+      if (response.ok) {
+        console.log('OTP code sent successfully!');
+      } else {
+        console.log('Failed to send OTP code.');
+      }
+    } catch (error) {
+      console.log('Error sending OTP code:', error);
+    }
+
+  });
+
+  verifyOtpButton.addEventListener('click', function(e) {
+    e.preventDefault();
+  
+    const enteredOtp = otpInput.value;
+    if (enteredOtp.trim() === '') {
+      showErrorName('Username is required.');
+      return;
+    }
+  
+    if (enteredOtp === otpCode) {
+      console.log('OTP matched!');
+      section3.style.display = 'none';
+      section4.style.display = 'block';
+      saveDataToDatabase(nameInput.value, numberInput.value);
+    } else {
+      console.log('OTP not matched!');
+      showErrorOtp('OTP not matched!');
+    }
+  });
+  
+  async function saveDataToDatabase(name, number) {
+    try {
+      const response = await fetch('/store-data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          number: number,
+        }),
+      });
+  
+      if (response.ok) {
+        console.log('Data saved to database successfully!');
+      } else {
+        throw new Error('Failed to save data to database.');
+      }
+    } catch (error) {
+      console.log('Error saving data to database:', error);
+    }
+  }
+  
+
+  function generateOTP() {
+    const digits = '0123456789';
+    let otp = '';
+
+    for (let i = 0; i < 4; i++) {
+      otp += digits[Math.floor(Math.random() * 10)];
+    }
+
+    return otp;
+  }
+  
+  function validateNumber(number) {
+    const numberRegex = /^923\d{9}$/;
+    return numberRegex.test(number);
+  }
+  function validateName(username) {
+    const nameRegex = /^.{4,19}$/;
+    return nameRegex.test(username);
+  }
+  
+  function showErrorNumber(errorMessage) {
+    errorNumber.innerText = errorMessage;
+    errorNumber.style.display = 'block';
+  }
+  
+  function showErrorName(errorMessage) {
+    errorName.innerText = errorMessage;
+    errorName.style.display = 'block';
+  }
+
+  
+  function showErrorOtp(errorMessage) {
+    errorOtp.innerText = errorMessage;
+    errorOtp.style.display = 'block';
+  }
+
+  function hideErrorName() {
+    errorName.innerText = '';
+    errorName.style.display = 'none';
+  }
+  
+  function hideErrorNumber() {
+    errorNumber.innerText = '';
+    errorNumber.style.display = 'none';
+  }
+
+  
+});
---
+++ server/server.js
@@ -0,0 +1,132 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import twilio from 'twilio';
+import bodyParser from 'body-parser';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push, set } from 'firebase/database';
+import useragent from 'express-useragent';
+import ngrok from 'ngrok';
+import os from 'os';
+import macaddress from 'macaddress';
+import axios from 'axios';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(useragent.express());
+
+// Firebase initialize
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+// Twilio
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+// Retrieve public IP address
+async function getPublicIpAddress() {
+  try {
+    const response = await axios.get("https://api.ipify.org?format=json");
+    return response.data.ip;
+  } catch (error) {
+    console.error("Error retrieving public IP address:", error);
+    throw error;
+  }
+}
+
+// Start ngrok to create a tunnel
+ngrok.connect(port)
+  .then(async (url) => {
+    console.log(`Server is running on ${url}`);
+
+    // Send OTP by Twilio
+    app.post('/send-otp', (req, res) => {
+      const { number, code } = req.body;
+
+      client.messages
+        .create({
+          body: `Your verification code is: ${code}`,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: `+${number}`,
+        })
+        .then((message) => {
+          console.log(message.sid);
+          res.sendStatus(200); // Sending 200 OK status to indicate success
+        })
+        .catch((error) => {
+          console.log('Error sending SMS:', error);
+          res.sendStatus(500); // Sending 500 Internal Server Error status to indicate failure
+        });
+    });
+
+    // Store Data in Firebase Realtime Database
+    app.post('/store-data', async (req, res) => {
+      const { name, number } = req.body;
+      
+      // Retrieve public IP address
+      const ipAddress = await getPublicIpAddress();
+
+      // Get MAC address
+      macaddress.one((err, macAddress) => {
+        if (err) {
+          console.error('Error retrieving MAC address:', err);
+          macAddress = 'Unknown';
+        }
+
+        // Get device name
+        const deviceName = os.hostname();
+
+        try {
+
+          // Generate a unique key for the data
+          const newDataKey = push(ref(database)).key;
+
+          // Create the data object
+          const newData = {
+            id: newDataKey,
+            name: name,
+            number: `+${number}`,
+            macAddress: macAddress,
+            deviceName: deviceName,
+            ipAddress: ipAddress,
+          };
+
+          // Store the data in the Firebase Realtime Database
+          set(ref(database, `users/${newDataKey}`), newData)
+            .then(() => {
+              console.log('Data stored in Firebase Realtime Database');
+              res.sendStatus(200); // Sending 200 OK status to indicate success
+            })
+            .catch((error) => {
+              console.error('Error storing data in Firebase Realtime Database:', error);
+              res.sendStatus(500); // Sending 500 Internal Server Error status to indicate failure
+            });
+        } catch (error) {
+          console.error('Error retrieving public IP address:', error);
+          res.sendStatus(500); // Sending 500 Internal Server Error status to indicate failure
+        }
+      });
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error starting ngrok:', error);
+  });
